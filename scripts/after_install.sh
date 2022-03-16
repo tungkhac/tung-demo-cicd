37cdb83f70fb704f91dd752246e244a5e73bbee4
unknown <tungnk.hn@gmail.com>
2022-03-16 17:18:45 +0700

    update

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/topnal_demo_cicd
+npm install
---
+++ scripts/application_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Stop all servers and start the server as a daemon
+forever stop /var/www/topnal_demo_cicd/bin/www
+forever start --uid "topnal_demo_cicd" /var/www/topnal_demo_cicd/bin/www
