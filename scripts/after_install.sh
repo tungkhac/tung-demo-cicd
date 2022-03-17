c17e68d76b1ad291b8f7407774afaef40fc5de50
unknown <tungnk.hn@gmail.com>
2022-03-17 12:39:01 +0700

    update code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,8 +3,8 @@
 #NVM_DIR=~/.nvm/nvm.sh
 #NVM_INSTALL_PATH=$NVM_DIR/versions/node/v$NODE_VERSION
 
-export NODE_PATH=~/.nvm/nvm.sh/versions/node/v12.6.0/lib/node_modules
-export PATH=~/.nvm/nvm.sh/versions/node/v12.6.0/bin:$PATH
+export NODE_PATH=~/.nvm/versions/node/v12.6.0/lib/node_modules
+export PATH=~/.nvm/versions/node/v12.6.0/bin:$PATH
 
 
 cd /var/www/topnal_demo_cicd
