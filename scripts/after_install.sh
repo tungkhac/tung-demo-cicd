a73b30df58a8262354f822de569ddecafade207b
unknown <tungnk.hn@gmail.com>
2022-03-17 13:30:31 +0700

    update code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ NVM_INSTALL_PATH_VAR=~/.nvm/versions/node/v$NODE_VERSION_VAR
 export NODE_PATH=$NVM_INSTALL_PATH_VAR/lib/node_modules
 export PATH=$NVM_INSTALL_PATH_VAR/bin:$PATH
 
-echo "-Set chmod"
-sudo chmod -R 755 /var/www/topnal_demo_cicd
+#echo "-Set chmod"
+#sudo chmod -R 755 /var/www/topnal_demo_cicd
 echo "-cd to project" 
 cd /var/www/topnal_demo_cicd
 
