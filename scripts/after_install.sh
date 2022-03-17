d119cc9551e2be280a7b78f6d5343dab85c3972e
unknown <tungnk.hn@gmail.com>
2022-03-17 11:53:21 +0700

    update code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NODE_VERSION="12.6.0"
-NVM_DIR="~/.nvm/nvm.sh"
-NVM_INSTALL_PATH $NVM_DIR/versions/node/v$NODE_VERSION
+NVM_DIR=~/.nvm/nvm.sh
+NVM_INSTALL_PATH=$NVM_DIR/versions/node/v$NODE_VERSION
 
 NODE_PATH $NVM_INSTALL_PATH/lib/node_modules
 PATH $NVM_INSTALL_PATH/bin:$PATH
