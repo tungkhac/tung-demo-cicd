d6e5437c2202117d1f78e0345715aac49cf78994
unknown <tungnk.hn@gmail.com>
2022-03-17 13:49:54 +0700

    update code

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,8 +4,9 @@ NVM_INSTALL_PATH_VAR=~/.nvm/versions/node/v$NODE_VERSION_VAR
 
 export PATH=$NVM_INSTALL_PATH_VAR/bin:$PATH
 
+cd /var/www/topnal_demo_cicd
 
 # Stop all servers and start the server as a daemon
 forever --version
 forever stop /var/www/topnal_demo_cicd/bin/www
-forever start --uid "topnal_demo_cicd" /var/www/topnal_demo_cicd/bin/www
+forever --uid "topnal_demo_cicd" --append start bin/www
