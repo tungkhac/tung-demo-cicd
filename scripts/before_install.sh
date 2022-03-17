ddcdf543e50b8c2d0b91588233a1d15309dc8ada
unknown <tungnk.hn@gmail.com>
2022-03-17 10:25:16 +0700

    update code

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Install nvm
 curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-source /usr/local/nvm/nvm.sh
+source ~/.nvm/nvm.sh
 nvm install 12.6.0
 nvm alias default 12.6.0
 nvm use default
