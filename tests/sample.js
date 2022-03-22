ceac4346f3e5424bf2342b4d6b126a97637880b7
nguyen.khac.tung <nguyen.khac.tung@topnal.com.vn>
2022-03-22 15:49:21 +0700

    update test

--- tests/sample.js
+++ tests/sample.js
@@ -23,30 +23,32 @@ describe('sample', () => {
     describe('/GET brand', () => {
         it('it should GET all brand', (done) => {
             chai.request(app)
-                .get('/brand')
-                .expect('Content-Type', /string/)
-                .expect(200)
-                .then((res) => {
-                    expect(res.body).to.be.a('string');
-                    let body = JSON.parse(res.body);
-                    expect(body).to.have.property('success', true);
-                    
-                    expect(body).to.have.property('data');
-                    expect(body).data.to.be.a('object');
-                    // expect(body).length.to.be.eql(9);
-                    expect(body).data.to.have.property('viettien, Việt Tiến');
-                });
-                /*.end((err, res) => {
-                    // res.should.have.status(200);
-                    // res.body.should.be.a('object');
-                    res.body.should.have.property('success').eql('true');
-                    
-                    res.body.should.have.property('data');
-                    res.body.data.should.be.a('object');
-                    // res.body.length.should.be.eql(9);
-                    res.body.data.should.have.property('viettien').eql('Việt Tiến');
-                    done();
-                });*/
+            .get('/brand')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                // expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                
+                expect(res.body).to.be.a('string');
+                let body = JSON.parse(res.body);
+                expect(body).to.have.property('success', true);
+                
+                expect(body).to.have.property('data');
+                expect(body).data.to.be.a('object');
+                // expect(body).length.to.be.eql(9);
+                expect(body).data.to.have.property('viettien, Việt Tiến');
+                // done();
+            });
+            /*.end((err, res) => {
+                // res.should.have.status(200);
+                // res.body.should.be.a('object');
+                res.body.should.have.property('success').eql('true');
+                
+                res.body.should.have.property('data');
+                res.body.data.should.be.a('object');
+                // res.body.length.should.be.eql(9);
+                res.body.data.should.have.property('viettien').eql('Việt Tiến');
+                done();
+            });*/
         });
     });
 });
